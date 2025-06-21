e11b2ac4ba54a247103d9f8f496b5a0ae075afb3
nikusha09 <nikusha.bakhtadze123@gmail.com>
2025-06-22 01:02:02 +0400

    merge parse endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import parsingRoutes from './routes/parsingRoutes';
 
 const app = express();
+app.use(express.json());
+
+app.use('/api', parsingRoutes);
+
 const PORT = 3000;
-
-app.get('/', (_req, res) => {
-  res.send('Hello from backend!');
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
